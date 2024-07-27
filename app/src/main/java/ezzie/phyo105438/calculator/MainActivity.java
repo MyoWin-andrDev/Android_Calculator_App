d3aa8ec6dd3fe2305473cb8b6d0b2c03b1073042
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-07-27 10:10:52 +0700

    Android_Calculator

--- app/src/main/java/ezzie/phyo105438/calculator/MainActivity.java
+++ app/src/main/java/ezzie/phyo105438/calculator/MainActivity.java
@@ -1,20 +1,33 @@
 package ezzie.phyo105438.calculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import ezzie.phyo105438.calculator.databinding.ActivityBlueBinding;
+import ezzie.phyo105438.calculator.databinding.ActivityRedBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityRedBinding redBinding;
+    private ActivityBlueBinding blueBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        blueBinding = ActivityBlueBinding.inflate(getLayoutInflater());
+        redBinding = ActivityRedBinding.inflate(getLayoutInflater());
+        setContentView(blueBinding.getRoot());
 
+    }
+
+    //On Number Clicked Btn
+    public void OnNumberClicked(View view){
+        Button btn = (Button) view;
+        String text = btn.getText().toString();
+        blueBinding.etCalculate.setText(text);
     }
 }
